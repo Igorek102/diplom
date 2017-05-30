6af476209b249a04fd5a065b63633cb36de4a2dc
igorek102 <igorek102161@gmail.com>
2017-05-30 14:33:11 +0300

    ниче особенного

---
+++ diplom/core/src/main/java/ru/igorek/core/Main.java
@@ -0,0 +1,16 @@
+package ru.igorek.core;
+
+
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Игорек
+ */
+public class Main {
+    private static Logger log = Logger.getLogger(Main.class);
+    
+    public static void main(String[] args) {
+        log.debug("asd");
+    }
+}
